be021ffa16e8e00938371a6b3bf44d01b9843ca4
rayaabudaia <raya662003@gmail.com>
2026-03-08 00:26:18 +0200

    Fix formatting with Prettier

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,20 +1,34 @@
 import { Request, Response, NextFunction } from "express";
 import { getAPIKey } from "./auth.js";
 
-export function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+export function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   res.on("finish", () => {
     if (res.statusCode >= 400) {
-      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
+      );
     }
   });
   next();
 }
 
-export function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+export function middlewareMetricsInc(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   next();
 }
 
-export function middlewareAuth(req: Request, res: Response, next: NextFunction) {
+export function middlewareAuth(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
     const apiKey = getAPIKey(req);
     (req as any).apiKey = apiKey;
